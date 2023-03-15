37c14172737b0c9166bd906b6bed016b3cb33e31
Iván López <ivanlopez@logosur.com>
2023-03-15 20:12:59 +0100

    complete tests

--- tests/ApplicationTest/ApiTest.php
+++ tests/ApplicationTest/ApiTest.php
@@ -3,14 +3,23 @@
 namespace App\Tests\ApplicationTest;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 class ApiTest extends WebTestCase
 {
-    public function testSomething(): void
+    public function testSuccesfulGet(): void
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/api/products?category=1');
 
         $this->assertResponseIsSuccessful();
+    }
+
+    public function testUnsuccesfulGet(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/api/products');
+
+        $this->assertNotEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
     }
 }
